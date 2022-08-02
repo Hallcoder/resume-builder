982d8d33e87459d8462f86dd132e832b1e16dc71
Hallcoder <hallcoder25@gmail.com>
2022-08-02 22:15:18 +0200

    skills section and bio

--- src/components/CollectInfo.tsx
+++ src/components/CollectInfo.tsx
@@ -6,13 +6,13 @@ const CollectInfo:React.FC = ()  => {
  return <div className="h-5/6 w-6/12 flex overflow-y-scroll flex-col">
     <form action="" className='h-5/6 ml-4 mr-2'>
         <Section title={'Personal Details'} RealSection={'PD'}/>
-        <Section title={'Bio'} />
-        <Section title={'Skills'} />
+        <Section title={'Bio'} RealSection={'BIO'}/>
+        <Section title={'Skills'} RealSection={'SKILL'} />
         <Section title={'Education'} />
         <Section title={'Employment'} />
-        <Section title={'Languages'} />
-        <Section title={'Languages'} />
-        <Section title={'Languages'} />
+        <Section title={'Personal Projects'} />
+        <Section title={'Hobbies'} />
+        <Section title={'Certificates'} />
     </form>
  </div>
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiOutlineDotsVertical } from 'react-icons/hi';
 import {MdKeyboardArrowDown} from 'react-icons/md'
 import PersonDetails from './Sections/PersonalDetails';
+import Bio from './Sections/Bio';
+import Skills from './Sections/Skills';
 
 interface Props{
 title:string,
@@ -10,6 +12,7 @@ RealSection?:string;
 }
 const Section:React.FC<Props> = ({title,RealSection}) => {
      let [styles,setStyles] = useState({display:'none'});
+     let [classAccording,setClassAccording] = useState('w-full h-[70vh]');
      const handleDisplay = () => { 
           if(styles.display === 'none'){
                setStyles({display:'flex'});
@@ -17,16 +20,30 @@ const Section:React.FC<Props> = ({title,RealSection}) => {
                setStyles({display:'none'})
           }
      }
+   useEffect(()=>{
+     switch(RealSection){
+          case 'BIO':
+               setClassAccording('w-full h-[40vh]')
+          break;
+          case 'PD':
+               setClassAccording('w-full h-[70vh]')
+          break;
+          default:
+               setClassAccording('w-full h-[40vh]')
+     }
+   },[])
 return <div className="flex w-full flex-col justify-between  min-h-fit  border-b-2 items-center">
      <div className="w-full p-5 h-full flex justify-between items-center">
           <h3 className="font-semibold text-lg m-1">{title}</h3>
-          <div className="">
-               <HiOutlineDotsVertical className="hidden"/>
+          <div className="flex items-center">
+               <HiOutlineDotsVertical className="text-3xl m-2 p-1 border rounded-md hover:border-blue-300"/>
                <MdKeyboardArrowDown className='text-3xl  border rounded-md ' onClick={handleDisplay} />
           </div>
      </div>
-     <div style={styles} className='w-full h-[70vh]'>
+     <div style={styles} className={classAccording}>
      {RealSection && RealSection === 'PD' && <PersonDetails />}
+     {RealSection && RealSection === 'BIO' && <Bio />}
+     {RealSection && RealSection === 'SKILL' && <Skills />}
      </div>
 </div>
 }
