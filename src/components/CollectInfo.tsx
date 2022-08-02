ff1693f6f0e128e360a40855143a813856beca6a
Hallcoder <hallcoder25@gmail.com>
2022-08-02 11:55:03 +0200

    added Personal Details section

--- src/components/CollectInfo.tsx
+++ src/components/CollectInfo.tsx
@@ -3,9 +3,9 @@ import Section from './Section';
 import PersonDetails from './Sections/PersonalDetails';
 
 const CollectInfo:React.FC = ()  => {
- return <div className="h-5/6 flex overflow-y-scroll flex-col w-6/12">
+ return <div className="h-5/6 w-6/12 flex overflow-y-scroll flex-col">
     <form action="" className='h-5/6 ml-4 mr-2'>
-        <Section title={'Personal Details'} realSection={<PersonDetails />}/>
+        <Section title={'Personal Details'} RealSection={'PD'}/>
         <Section title={'Bio'} />
         <Section title={'Skills'} />
         <Section title={'Education'} />
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
 import {MdKeyboardArrowDown} from 'react-icons/md'
+import PersonDetails from './Sections/PersonalDetails';
 
 interface Props{
 title:string,
 //pay attention here later if you meet errors
-realSection?:React.FunctionComponentElement<React.FC>;
-
+RealSection?:string;
 }
-const Section:React.FC<Props> = ({title}) => {
-return <div className="flex justify-between h-[13vh]  border-b-2 items-center">
-     <h3 className="font-semibold text-lg m-1">{title}</h3>
-     <MdKeyboardArrowDown className='text-3xl  border rounded-md ' />
+const Section:React.FC<Props> = ({title,RealSection}) => {
+     let [styles,setStyles] = useState({display:'none'});
+     const handleDisplay = () => { 
+          if(styles.display === 'none'){
+               setStyles({display:'flex'});
+          }else{
+               setStyles({display:'none'})
+          }
+     }
+return <div className="flex w-full flex-col justify-between  min-h-fit  border-b-2 items-center">
+     <div className="w-full p-5 h-full flex justify-between items-center">
+          <h3 className="font-semibold text-lg m-1">{title}</h3>
+          <div className="">
+               <HiOutlineDotsVertical className="hidden"/>
+               <MdKeyboardArrowDown className='text-3xl  border rounded-md ' onClick={handleDisplay} />
+          </div>
+     </div>
+     <div style={styles} className='w-full h-[70vh]'>
+     {RealSection && RealSection === 'PD' && <PersonDetails />}
+     </div>
 </div>
 }
 
