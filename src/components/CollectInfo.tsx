b385dc538ccac8659c55a1569de911a543f89ab5
Hallcoder <hallcoder25@gmail.com>
2022-08-02 08:36:46 +0200

    the real section with the first one being personal details

--- src/components/CollectInfo.tsx
+++ src/components/CollectInfo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Section from './Section';
+import PersonDetails from './Sections/PersonalDetails';
 
 const CollectInfo:React.FC = ()  => {
  return <div className="h-5/6 flex overflow-y-scroll flex-col w-6/12">
     <form action="" className='h-5/6 ml-4 mr-2'>
-        <Section title={'Personal Details'} />
+        <Section title={'Personal Details'} realSection={<PersonDetails />}/>
         <Section title={'Bio'} />
         <Section title={'Skills'} />
         <Section title={'Education'} />
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,6 +3,9 @@ import {MdKeyboardArrowDown} from 'react-icons/md'
 
 interface Props{
 title:string,
+//pay attention here later if you meet errors
+realSection?:React.FunctionComponentElement<React.FC>;
+
 }
 const Section:React.FC<Props> = ({title}) => {
 return <div className="flex justify-between h-[13vh]  border-b-2 items-center">
