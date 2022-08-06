0447c89b24a88b5da1743ef1469fba3cb26dd84e
Hallcoder <hallcoder25@gmail.com>
2022-08-06 09:17:35 +0200

    set store in accordance to bio section setup

--- src/redux/actionTypes.ts
+++ src/redux/actionTypes.ts
@@ -1,1 +1,7 @@
-export const 
+export const SET_NAME: string = "setName";
+export const SET_FAMILY_NAME: string = "setFamilyName";
+export const SET_EMAIL: string = "setEmail";
+export const SET_HEAD: string = "setHead";
+export const SET_PHONE:string = "setPhone";
+export const SET_ADDRESS:string = "setAddress";
+export const SET_LINKEDIN: string = "setLinkedin";
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,4 +1,23 @@
-const reducer:(state:Object,action:Object) => Object = (state:Object={},action:{}) => {
- return {}
+import * as actions from '../redux/actionTypes'
+
+interface State{
+    names?:string,
+    familyName?:string,
+    address?:string,
+    email?:string,
+    headLine?:string,
+    phone?:string,
+    linkedIn?:string,
+}
+
+const reducer:(state:State,action:{type:string,payLoad:Object}) => Object = (state:State={},action:{type:string,payLoad:Object}) => {
+     let currentState:State = {...state};
+    switch(action.type){
+    case actions.SET_ADDRESS:
+          currentState.address = (action.payLoad as State).address;
+    return currentState;
+    default:
+        return state;
+ }
 }
 export default  reducer;
