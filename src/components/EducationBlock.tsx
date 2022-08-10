e814ea05dfa2890d0ade5cb27818b5daef499813
Hallcoder <hallcoder25@gmail.com>
2022-08-10 17:29:38 +0200

    Removed ts checking in some files

---
+++ src/components/EducationBlock.tsx
@@ -0,0 +1,46 @@
+ // @ts-nocheck
+import React, { RefObject, useState } from "react";
+import { MdModeEdit } from "react-icons/md";
+import SkillAndLanguageForm from "./Sections/SkillAndLanguageForm";
+import State from './../utils/interfaces';
+import { useDispatch } from 'react-redux';
+import Form from "./Form";
+interface Props{
+title: string,
+value?:string,
+level?: string,
+skill?:State,
+onDone?:Function,
+subtitle?:string,
+}
+const EducationBlock: React.FC<Props> = ({title, subtitle,value,skill,onDone}) => {
+  const [status, setStatus] = useState("set");
+  const edInputs:string [] = ['Degree','School','City','Start Date','End Date','Descripion']
+  const dispatch = useDispatch();
+  const handleChangeStatus = (status:string) => {
+    setStatus(status)
+  }
+  if (status === "set") {
+    return (
+      <div className="flex w-11/12  h-[10vh] ml-auto mr-auto m-1 mt-1 items-center border border-gray-400 shadow-md rounded-sm">
+        <div className="w-6/12 m-1">
+          <h1>{title}</h1>
+          <h5 className="text-sm mt-2 text-gray-500">{subtitle}</h5>
+        </div>
+        <div className="rounded-sm w-6/12 flex justify-end">
+          <MdModeEdit className="border text-3xl rounded-md p-1 m-1" onClick={() => handleChangeStatus('edit')}/>
+        </div>
+      </div>
+    );
+  } else if (status === "edit") {
+    return <div>
+    <Form name={'Skill'}  inputs={edInputs} />
+    </div>;
+  } else {
+    return <div>
+
+    </div>;
+  }
+};
+
+export default EducationBlock;
