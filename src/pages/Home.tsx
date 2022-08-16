2c31cba953a0bb149e04ac21b4a147c557e8a243
Hallcoder <hallcoder25@gmail.com>
2022-08-16 20:13:35 +0200

    finished tools

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Intro from "../components/Intro";
 import NavBar from "../components/NavBar";
 import Templates from "../components/Templates";
 import Reviews from './../components/Review';
+import Tools from './../components/Tools';
 function Home() {
   return (
     <div className="w-full border-red-400 ">
@@ -11,6 +12,7 @@ function Home() {
         <Intro />
         <Reviews />
         <Templates />
+        <Tools />
       </div>
     </div>
   );
